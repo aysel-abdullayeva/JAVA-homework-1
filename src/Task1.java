8c7af33c39453d18371339b6915485e55476235b
aysel-abdullayeva <nemat.abdullayev@machinarium.co>
2026-06-19 16:01:29 +0400

    Task1 done

---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args) {
+        String customerName = "Karim Karimov";
+        int accountNumber = 9999;
+        double balance = 9999.99;
+        boolean isActive = true;
+        String accountType = "Savings";
+
+        System.out.println("Customer:       " + customerName);
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("Balance:        " + balance);
+        System.out.println("Is active:      " + isActive);
+        System.out.println("Account type:   " + accountType);
+    }
+}

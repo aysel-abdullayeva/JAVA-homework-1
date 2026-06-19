a0c1d41e9b2e2bc49b188bc4d51e41d000797ce4
aysel-abdullayeva <nemat.abdullayev@machinarium.co>
2026-06-19 18:19:51 +0400

    Task11 done

---
+++ src/Task11.java
@@ -0,0 +1,14 @@
+public class Task11 {
+    public static void main(String[] args) {
+        int x = 18, y = 18, z = 25;
+
+        System.out.println("x == y → " + (x == y));
+        System.out.println("x == z → " + (x == z));
+        System.out.println("x != z → " + (x != z));
+        System.out.println("x < z  → " + (x < z));
+        System.out.println("z > x  → " + (z > x));
+        System.out.println("x >= y → " + (x >= y));
+        System.out.println("y <= z → " + (y <= z));
+
+    }
+}

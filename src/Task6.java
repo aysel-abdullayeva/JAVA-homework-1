93669eea7e325e44621cb830f1aaaa7551b76cdc
aysel-abdullayeva <nemat.abdullayev@machinarium.co>
2026-06-19 17:10:07 +0400

    Task6 done

---
+++ src/Task6.java
@@ -0,0 +1,12 @@
+public class Task6 {
+    public static void main(String[] args) {
+        int a = 17, b = 5;
+
+        System.out.println("Toplama:   " + a + " + " + b + " = " + (a + b));
+        System.out.println("Çıxma:     " + a + " - " + b + " = " + (a - b));
+        System.out.println("Vurma:     " + a + " * " + b + " = " + (a * b));
+        System.out.println("Bölmə:     " + a + " / " + b + " = " + (a / b));
+        System.out.println("Qalıq:     " + a + " % " + b + " = " + (a % b));
+
+    }
+}

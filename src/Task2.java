7104c9f2ce82a53fd8b4afd7a283d850c19b54d8
aysel-abdullayeva <nemat.abdullayev@machinarium.co>
2026-06-19 16:20:11 +0400

    Task2 done

---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int bal = 1000;
+        System.out.println("Başlanğıc: " + bal);
+
+        bal = bal + 500;
+        System.out.println("500 əlavə: " + bal);
+
+        bal -= 200;
+        System.out.println("200 çıxdı: " + bal);
+
+        bal *= 2;
+        System.out.println("2 vuruldu: " + bal);
+
+        bal /= 4;
+        System.out.println("4-ə bölündü: " + bal);
+    }
+}

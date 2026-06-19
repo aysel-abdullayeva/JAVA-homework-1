9b6ea41ace8bde834ebf7dc9a125223c5388c336
aysel-abdullayeva <nemat.abdullayev@machinarium.co>
2026-06-19 17:03:25 +0400

    Task5 done

---
+++ src/Task5.java
@@ -0,0 +1,17 @@
+public class Task5 {
+    public static void main(String[] args) {
+        char bashErf = 'A';
+        String tamAd = "Karim Karimov";
+        int yas = 28;
+        double ayliqGelir = 999.99;
+        boolean vip = true;
+        int musteriKodu = 10245;
+
+        System.out.println("Baş hərf:      " + bashErf);
+        System.out.println("Tam ad:        " + tamAd);
+        System.out.println("Yaş:           " + yas);
+        System.out.println("Aylıq gəlir:   " + ayliqGelir + " AZN");
+        System.out.println("VIP müştəri:   " + vip);
+        System.out.println("Müştəri kodu:  " + musteriKodu);
+    }
+}
